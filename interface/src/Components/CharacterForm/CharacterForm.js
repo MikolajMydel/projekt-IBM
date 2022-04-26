f34892585c8bd22ba2ed2b3b4f77d1a74944bd35
MikolajMydel <mydelmikolaj@gmail.com>
2022-04-26 14:36:36 +0200

    Components

---
+++ interface/src/Components/CharacterForm/CharacterForm.js
@@ -0,0 +1,140 @@
+import {eyeColors, skinColors, hairColors, races} from "./datalists.js";
+import styles from "./CharacterForm.module.scss";
+import react from 'react';
+
+class CharacterForm extends react.Component {
+    render() {
+        return (
+            <form action="." method="GET"
+                className={styles.Form}
+            >
+                <div>
+                    <input type="text"/>
+                    <label>
+                        Name
+                    </label>
+                </div>
+
+                <div>
+                    <input type="text"/>
+                    <label>
+                        Gender
+                    </label>
+                </div>
+
+                <div>
+                    <input type="text" list="eyeColor"/>
+                    <label>
+                        Eye color
+                    </label>
+
+                    <datalist id="eyeColor">
+                        {eyeColors.map((value) => {
+                            return <option value={value.toLowerCase()} />
+                        })}
+                    </datalist>
+                </div>
+
+                <div>
+                    <input type="text" list="race" />
+                    <label>
+                        Race
+                    </label>
+
+                    <datalist id="race">
+                        {races.map((value) => {
+                            return <option value={value.toLowerCase()} />
+                        })}
+                    </datalist>
+                </div>
+
+                <div>
+                    <input type="text" list="hairColor" />
+                    <label>
+                        Hair color
+                    </label>
+
+                    <datalist id="hairColor">
+                        {hairColors.map((value) => {
+                                return <option value={value.toLowerCase()} />
+                        })}
+                    </datalist>
+                </div>
+
+                <div>
+                    <input type="text" list="skinColor"/>
+                    <label>
+                        Skin color
+                    </label>
+
+                    <datalist id="skinColor">
+                        {skinColors.map((value) => {
+                                return <option value={value.toLowerCase()} />
+                        })}
+                    </datalist>
+                </div>
+
+                <div>
+                    <input type="number"/>
+                    <label>
+                        Height
+                    </label>
+                </div>
+
+                <div>
+                    <input type="number"/>
+                    <label>
+                        Weight
+                    </label>
+                </div>
+
+                <div>
+                    <input type="range"/>
+                    <label>
+                        Intelligence
+                    </label>
+                </div>
+
+                <div>
+                    <input type="range"/>
+                    <label>
+                        Strength
+                    </label>
+                </div>
+
+                <div>
+                    <input type="range"/>
+                    <label>
+                        Speed
+                    </label>
+                </div>
+
+                <div>
+                    <input type="range"/>
+                    <label>
+                        Durability
+                    </label>
+                </div>
+
+                <div>
+                    <input type="range"/>
+                    <label>
+                        Power
+                    </label>
+                </div>
+
+                <div>
+                    <input type="range"/>
+                    <label>
+                        Combat
+                    </label>
+                </div>
+
+                <input type="submit" value="Check">
+                </input>
+            </form>
+        )
+    }
+}
+
+export default CharacterForm;
