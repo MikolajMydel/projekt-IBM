443f1ae0018e07392f027eb6d50e4bebcd46e9c3
MikolajMydel <mydelmikolaj@gmail.com>
2022-04-26 11:45:40 +0200

    Form component

---
+++ interface/src/Components/Form/Form.js
@@ -0,0 +1,129 @@
+import {eyeColors, skinColors, hairColors, races} from "./datalists.js";
+
+const Form = () => (
+    <form action="." method="GET">
+        <div>
+            <input type="text"/>
+            <label>
+                Name
+            </label>
+        </div>
+
+        <div>
+            <input type="text"/>
+            <label>
+                Gender
+            </label>
+        </div>
+
+        <div>
+            <input type="text" list="eyeColor"/>
+            <label>
+                Eye color
+            </label>
+
+            <datalist id="eyeColor">
+                {eyeColors.map((value) => {
+                    return <option value={value.toLowerCase()} />
+                })}
+            </datalist>
+        </div>
+
+        <div>
+            <input type="text" list="race" />
+            <label>
+                Race
+            </label>
+
+            <datalist id="race">
+                {races.map((value) => {
+                    return <option value={value.toLowerCase()} />
+                })}
+            </datalist>
+        </div>
+
+        <div>
+            <input type="text" list="hairColor" />
+            <label>
+                Hair color
+            </label>
+
+            <datalist id="hairColor">
+                {hairColors.map((value) => {
+                        return <option value={value.toLowerCase()} />
+                })}
+            </datalist>
+        </div>
+
+        <div>
+            <input type="text" list="skinColor"/>
+            <label>
+                Skin color
+            </label>
+
+            <datalist id="skinColor">
+                {skinColors.map((value) => {
+                        return <option value={value.toLowerCase()} />
+                })}
+            </datalist>
+        </div>
+
+        <div>
+            <input type="number"/>
+            <label>
+                Height
+            </label>
+        </div>
+
+        <div>
+            <input type="number"/>
+            <label>
+                Weight
+            </label>
+        </div>
+
+        <div>
+            <input type="range"/>
+            <label>
+                Intelligence
+            </label>
+        </div>
+
+        <div>
+            <input type="range"/>
+            <label>
+                Strength
+            </label>
+        </div>
+
+        <div>
+            <input type="range"/>
+            <label>
+                Speed
+            </label>
+        </div>
+
+        <div>
+            <input type="range"/>
+            <label>
+                Durability
+            </label>
+        </div>
+
+        <div>
+            <input type="range"/>
+            <label>
+                Power
+            </label>
+        </div>
+
+        <div>
+            <input type="range"/>
+            <label>
+                Combat
+            </label>
+        </div>
+    </form>
+)
+
+export default Form;
---
+++ interface/src/Components/Form/datalists.js
@@ -0,0 +1,22 @@
+const eyeColors = ['yellow', 'green', 'blue', 'brown', '-', 'red', 'violet', 'white',
+    'purple', 'black', 'silver', 'grey', 'yellow (without irises)',
+    'blue / white', 'hazel', 'green / blue', 'gold', 'white / red',
+    'amber', 'yellow / blue'];
+
+const races = ["Human", "Human / Radiation", "Cosmic Entity",
+    "Cyborg", "Mutant", "Symbiote", "Alien", "Neyaphem",
+    "God / Eternal", "Inhuman", "Demon", "Vampire", "Human-Kree",
+    "Human / Altered", "Kakarantharaian", "Zen-Whoberian", "Strontian", "Flora Colossus", "Asgardian",
+    "Demi-God", "Eternal", "Spartoi", "Luphomoid", "Animal", "Clone", "Human-Spartoi",
+    "Android", "Frost Giant", "Mutant / Clone"];
+
+const hairColors = ['No Hair', 'Black', 'Blond', 'Brown', '-', 'White', 'Purple',
+    'Orange', 'Pink', 'Red', 'Strawberry Blond', 'Blue', 'Auburn',
+    'Magenta', 'Brown / Black', 'Brown / White', 'blond', 'Green',
+    'black', 'Grey', 'Orange / White', 'Yellow', 'Brownn', 'Gold',
+    'Red / Orange', 'Indigo', 'Silver', 'Red / White'];
+
+const skinColors = ['grey', 'blue', 'gold', 'red', 'white', 'pink', 'green',
+    'purple', 'silver', 'yellow'];
+
+export {eyeColors, skinColors, hairColors, races};
