cb79164d68eb5a50426b7a02e9a06134412fb183
MikolajMydel <mydelmikolaj@gmail.com>
2022-04-28 17:12:09 +0200

    Express app setup

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,11 +1,34 @@
+import React from "react";
 import CharacterForm from "./Components/CharacterForm/CharacterForm.js";
 
-function App() {
-  return (
-    <>
-    <CharacterForm />
-    </>
-  );
+class App extends React.Component {
+  state = {
+    data: null,
+
+  };
+
+  componentDidMount(){
+    this.callBackendAPI()
+    .then( res => this.setState({"data": res.express }))
+    .catch( err => console.log(err) );
+  }
+
+  callBackendAPI = async() => {
+    const response = await fetch("/express-backend");
+    const body = await response.json();
+
+    if (response.status !== 200)
+      throw Error(body.message);
+
+    return body;
+  }
+  render(){
+    return (
+      <>
+        <CharacterForm />
+      </>
+    );
+  }
 }
 
 export default App;
